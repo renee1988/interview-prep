d35e4fb13f86519266a14f0185394e444d41a58b
Juyang Xia <juyang.xia33@gmail.com>
2021-01-15 22:28:23 -0800

    Added google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+ReactGA.initialize('UA-178487778-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
